c5c7f328756594e9cb2111bc8713aaf0966f1117
smuneera253 <smuneera253@gmail.com>
2023-06-19 03:12:37 +0530

    compliting

---
+++ client-side(frontend)/capstone-project/src/component/Touristplace.js
@@ -0,0 +1,212 @@
+import React from "react";
+import './Touristplace.css'
+
+function Tourist(){
+    return(
+        <div>
+            
+            <nav className="navbar">
+               <div className="line1">
+                   <select id="cars">
+                           <option value="saab">Urdu</option>
+                           <option value="vw">Telugu</option>
+                           <option value="audi" selected>Hindi</option>
+                           <option value="audi" selected>English</option>
+                   </select>
+                   <div className="skip"><p>skip main content |</p></div>
+
+                   <div className="Dark">
+                       <p className="mood">Dark Mode |</p>
+
+                   </div>
+                   
+                        {/* <p className="about">about</p>
+                    */}
+            </div>
+                   <div className="flex">
+                           <img src="resources/flag.png" className="img2"/>
+                           <img src="resources/govtlogo.png" className="img1"/>
+                            <input type="text" Placeholder="search" className="search"></input><br></br>
+                           <img src="resources/search1.png" className="btn"/>
+                           {/* <div className="image"></div> */}
+                           <span className="sign11">sign up</span>
+                           <span className="login">login</span>
+                           <div className="login">about</div>
+                          
+                   </div>
+                   <hr/>
+                   <div className="nav">
+                       {/* <span img src="resources/navbarc.png" className="img3">Topic</span> */}
+
+                       <div>
+                               <select className="back bottom">
+                                {/* <div>
+                                    <img className="whitetopic" src="resources/Topic.png"/>
+                               </div> */}
+                                   <option value="bottom">Topics</option>
+                                   <option value="option1 ">poltical leader</option>
+                                   <option value="option2">Government budjet</option>
+                                   <option value="option3">Government plans</option>
+                                   <option value="option3">Government office numbers</option>
+                                   <option value="option3">indian most beatiful torist place</option>
+                                   <option value="option3">Help_line number</option>
+                               </select>
+                               </div>
+                       <span img src="resources/search1.png" className="poltical bottom">Poltical leaders</span>
+                       <span className="poltical bottom">Government budget</span>
+                       <span className="poltical bottom">Government plans</span>
+                       <span className="poltical bottom">Government office number</span>
+                       <span className="poltical bottom">Tourist place</span>
+                       <span className="poltical bottom">Help_line numbers</span>
+                   </div>
+                 
+            </nav>
+            <div>
+                <marquee>
+        
+                <p className="phara">Our website provides comprehensive  information on government-related topics, including details about the current Prime Minister, their accomplishments, and performance in office. We also offer insights into government benefit plans and loans available to citizens, along with a clear hierarchy of government officials. Additionally, we provide contact numbers for various government offices, ensuring easy access for any inquiries or assistance needed. Lastly, our website features valuable information on popular tourist destinations, including relevant contact numbers, enabling visitors to plan their trips effectively.</p>
+                </marquee>
+            </div>
+            <div className="mainbox">
+              <div className="mainbox1">
+                <div className="firstbox">
+                            <img src="/resources/Budget1.png" className="dolar"/>
+                            <h2 className="boderbottom">Tourist Place </h2>
+                            
+                        </div> 
+                        <div className="tourist">
+                            <ul>
+                                <li><p  className="redcolor purple">Andhara Pradesh</p></li>
+                                <li ><p className="redcolor purple">Kernataka</p></li>
+                                <li><p  className="redcolor purple">Tamil Nadu</p></li>
+                                <li><p  className="redcolor purple">Goa</p></li>
+                                <li><p  className="redcolor purple">Delhi</p></li>
+                                <li><p  className="redcolor purple">Punjab</p></li>
+                                <li><p  className="redcolor purple">Gujarat</p></li>
+                                <li><p  className="redcolor purple">Rajastan</p></li>
+                                <li><p  className="redcolor purple">Odisha</p></li>
+                                <li><p  className="redcolor purple">Jammu and kashmir</p></li>
+                                <li><p  className="redcolor purple">Kolkata</p></li>
+                                <li><p  className="redcolor purple">Madhya Pradesh</p></li>
+                                <li><p  className="redcolor purple">Kerala</p></li>
+                                <li><p  className="redcolor purple">Maharashtra</p></li>
+                                <li><p  className="redcolor purple">Telangana</p></li>
+                                <li><p  className="redcolor purple">Utter Pradesh</p></li>
+                                <li><p  className="redcolor purple">Sikkim</p></li>
+                                <li><p  className="redcolor purple">Himalchal Pradesh</p></li>
+                            </ul>
+                        </div>
+                 
+                </div>
+                <div className="mainbox2">
+                    <div className="Tplace">
+                        <img src="resources/Indiagate.jpg" className=" indiagate"/>
+                        <div >
+                            <h2 className="gate">Indiagate</h2>
+                           <ul>
+                            <li className="thisgate"> <p >This 42 meter high "Arc-de-Triomphe" like archway situated in the heart of Delhi city is much taller in the hearts of India.</p></li>
+                            <li className="thisgate"><p>Formerly known as All India War Memorial, India Gate was built to commemorate the death of 70,000 British Indian soldiers who died during First World War between 1914-21.</p></li>
+                            <li className="thisgate"><p>Even though the India Gate was constructed as a war memorial, the architectural style of the monument makes it appear like an epitome of triumph.</p></li>
+                            <li className="thisgate"><p> Much similar to Arch of Constantine, outside the Colosseum in Rome, India gate was designed by Sir Edward Lutyens.</p></li>
+                           </ul>
+                         </div>
+                       
+                       
+                    </div>
+                    <div>
+                        <div className="mainbox3">
+                                
+                            <div className="RedFort">
+                                <img src="resources/Indiagate.jpg" className=" indiagate"/>
+                                <div >
+                                    <h2 className="gate">Indiagate</h2>
+                                    <ul>
+                                        <li className="thisgate"> <p >This 42 meter high "Arc-de-Triomphe" like archway situated in the heart of Delhi city is much taller in the hearts of India.</p></li>
+                                        <li className="thisgate"><p>Formerly known as All India War Memorial, India Gate was built to commemorate the death of 70,000 British Indian soldiers who died during First World War between 1914-21.</p></li>
+                                        <li className="thisgate"><p>Even though the India Gate was constructed as a war memorial, the architectural style of the monument makes it appear like an epitome of triumph.</p></li>
+                                        <li className="thisgate"><p> Much similar to Arch of Constantine, outside the Colosseum in Rome, India gate was designed by Sir Edward Lutyens.</p></li>
+                                    </ul>
+                                </div>
+                        
+                             </div>
+                             <div className="mainbox3">
+                                
+                                <div className="RedFort">
+                                    <img src="resources/Indiagate.jpg" className=" indiagate"/>
+                                    <div >
+                                        <h2 className="gate">Indiagate</h2>
+                                        <ul>
+                                            <li className="thisgate"> <p >This 42 meter high "Arc-de-Triomphe" like archway situated in the heart of Delhi city is much taller in the hearts of India.</p></li>
+                                            <li className="thisgate"><p>Formerly known as All India War Memorial, India Gate was built to commemorate the death of 70,000 British Indian soldiers who died during First World War between 1914-21.</p></li>
+                                            <li className="thisgate"><p>Even though the India Gate was constructed as a war memorial, the architectural style of the monument makes it appear like an epitome of triumph.</p></li>
+                                            <li className="thisgate"><p> Much similar to Arch of Constantine, outside the Colosseum in Rome, India gate was designed by Sir Edward Lutyens.</p></li>
+                                        </ul>
+                                    </div>
+                            
+                                 </div>
+                                 
+                                <button className="click">view all</button>
+                                
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+            <div className="box6"><span className="back1">back to top</span></div>
+                       <div className="box7">
+                            <div className="aboutus">
+                                <div >
+                                    <h4>About</h4>
+                                    <p>Government information</p>
+                                    <p>Poltical leader</p>
+                                    <p>Government budjet</p>
+                                    <p>police department</p>
+                                </div>
+                                <div className="foter">
+                                    <h4>Government Plans</h4>
+                                    <p>loans</p>
+                                    <p>benefit plans for people</p>
+                                    <p>hierarchy information about Government</p>
+                                </div>
+                                <div className="foter">
+                                    <h4>Government office numbers</h4>
+                                    <p>Help_line numbers</p>
+                                    <p>agriculture</p>
+                                    <p>indian culture</p>
+                                    <p> indian cricket</p>
+                                    <p>army</p>
+                                   
+                                </div>
+                                <div className="foter">
+                                    <h4>Government Jobs notifications</h4>
+                                    <p>UPSC</p>
+                                    <p>NEET1</p>
+                                    <p>indian army jobs</p>
+                                    <p>CBI jobs</p>
+                                </div>
+                               
+                            </div>
+                            
+                            <hr/>
+                            <div className="icon2">
+
+                                  <img className="Twitter" src="resources/Twitter.png" alt="image"/>
+                                  <img className="linkdil" src="resources/linkdil.png" alt="image"/>
+                                  <div>
+                                   <img className="facebook1" src="resources/facebook.webp" alt="image"/>
+                                   </div>
+                            </div>
+                            <p className="this">This website offers comprehensive government information, job notifications, and insights on Indian culture, cricket, and agriculture.</p>
+                       </div>
+
+       </div>
+
+           
+        
+        
+    );
+}
+export default Tourist
